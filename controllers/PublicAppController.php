0f79a7fe93f5bcc8168e903becd283441eb267c2
QasimAbbas512 <35256158+QasimAbbas512@users.noreply.github.com>
2020-12-11 00:48:47 +0500

    add company projects in login service

--- controllers/PublicAppController.php
+++ controllers/PublicAppController.php
@@ -126,38 +126,77 @@ class PublicAppController extends Controller
 
         $client_info = ClientInfo::find()->where(['UserName' => $user])->andWhere(['Password' => $pass])->andWhere(['Active' => 'Y'])->andWhere(AppConstants::get_active_record_only)->one();
         if(!empty($client_info)) {
+            $company_info = CommonFunctions::selectCompanyBranchInfo($client_info->BranchID);
+            $company_projects = Projects::find()->where(['CompanyID' => $company_info->CompanyID])->andWhere(AppConstants::get_active_record_only)->all();
             $client_id = $client_info->ID;
-            $investment_info = ClientInvestment::find()->where(['ClientID' => $client_id])->andWhere(['Active' => 'Y'])->andWhere(AppConstants::get_active_record_only)->one();
+            $investment_info = ClientInvestment::find()->where(['ClientID' => $client_id])->andWhere(AppConstants::get_active_record_only)->one();
             $project_id = $investment_info->ProjectID;
             $project_info = Projects::find()->where(['ID' => $project_id])->andWhere(['Active' => 'Y'])->andWhere(AppConstants::get_active_record_only)->one();
+            $gallery_info = ProjectGallery::find()->where(['ImageFor'=>'android'])->andWhere(['ProjectID'=>$project_info->ID])->andwhere(AppConstants::get_active_record_only)->orderBy(['rand()' => SORT_DESC])->limit(1)->one();
 
             for ($i = 1; $i <= 3; $i++) {
                 $project_name = $project_info->ProjectName;
-                $validity = $investment_info->ContractExpiryDate;
-                $month_instive = 100000;
-                $t_inv = 10000000;
-                $t_date_inst = 400000;
-                $rcved = 300000;
-                $bln = 100000;
+                $validity = date('d M, Y',strtotime($investment_info->ContractExpiryDate));
+                $month_instive = number_format(100000,0);
+                $t_inv = number_format(10000000,0);
+                $t_date_inst = number_format(400000,0);
+                $rcved = number_format(300000,0);
+                $bln = number_format(100000,0);
+
                 if($i == 1){
                     $concat = '';
                     $project_name = 'Arcade-I';
                 }else{
                     $concat = $i;
                 }
-                if($i == 2){$project_name = 'Arcade-II';$validity = '2022-08-12'; $t_inv = 250000; $month_instive = 300000; $t_date_inst = 50000; $rcved = 700512;$bln = 102500;}
-                if($i == 3){$project_name = 'Arcade-III';$validity = '2026-12-31'; $t_inv = 1500000; $month_instive = 400000; $t_date_inst = 2503698; $rcved = 102590; $bln = 25000;}
-                $projects[] = array("Project" => $project_name, "ContractValidity" => $validity, "TotalInvestment" => $t_inv, "MonthlyInsentive" => $month_instive, "TillDateAmountIncentive" => $t_date_inst, "Recieved" => $rcved, "Balance" => $bln);
+                if($i == 1){ $projectid = 4;}
+                if($i == 2){ $projectid = 2;}
+                if($i == 3){ $projectid = 7;}
+                $gallery_info = ProjectGallery::find()->where(['ImageFor'=>'android'])->andWhere(['ProjectID'=>$projectid])->andwhere(AppConstants::get_active_record_only)->orderBy(['rand()' => SORT_DESC])->limit(1)->one();
+
+                $project_img_link = AppConstants::ProjectImgUrl.'/'.$gallery_info->ImageName;
+                if($i == 2){$project_name = 'Business Centre';$validity = '12 Aug, 2022'; $t_inv = number_format(250000,0); $month_instive = number_format(300000,0); $t_date_inst = number_format(50000,0); $rcved = number_format(700512,0);$bln = number_format(102500,0);}
+                if($i == 3){$project_name = 'OCTA';$validity = '31 Dec, 2026'; $t_inv = number_format(1500000,0); $month_instive = number_format(400000,0); $t_date_inst = number_format(2503698,0); $rcved = number_format(102590,0); $bln = number_format(25000,0);}
+                $projects[] = array("Project" => $project_name, "ContractValidity" => $validity, "TotalInvestment" => $t_inv, "MonthlyInsentive" => $month_instive, "TillDateAmountIncentive" => $t_date_inst, "Recieved" => $rcved, "Balance" => $bln,"ImageLink"=>$project_img_link);
             }
 
-            $client_profile_img = "http://aaacrm.net/cms/web/emp_images/202011241545245XUgrsa.jpg";
-            $company_profile_img = "http://aaacrm.net/cms/web/logo_image/20200827170716aaa_logo.png";
+            ///company projects/////
+            if(!empty($project_info)){
+                foreach($company_projects as $p){
+                    $Investment = 0;
+                    $MonthlyInsentive = 0;
+                    $TillDateAmountRcv = 0;
+                    if($p->ID == 2){
+                        $Investment = 100000;
+                        $MonthlyInsentive = 10000000;
+                        $TillDateAmountRcv = 300000;
+                    }
+                    if($p->ID == 5){
+                        $Investment = 250000;
+                        $MonthlyInsentive = 300000;
+                        $TillDateAmountRcv = 700512;
+                    }
+
+                    if($p->ID == 7){
+                        $Investment = 1500000;
+                        $MonthlyInsentive = 400000;
+                        $TillDateAmountRcv = 102590;
+                    }
+
+                    $company_active_projects[] = array("ProjectName"=>$p->ProjectName,"Investment"=>number_format($Investment,0),"MonthlyInsentive"=>number_format($MonthlyInsentive),"TillDateAmountRcv"=>number_format($TillDateAmountRcv));
+                }
+            }else{
+                $company_active_projects[] = array();
+            }
+
+            $client_profile_img = "http://aaacrm.net/aaacms/web/emp_images/blankM.jpg";
+            $company_profile_img = "http://aaacrm.net/aaacms/web/logo_image/20200827170716aaa_logo.png";
 
             $clientProfile = array("name" => ucfirst($client_info->FirstName), "LastName" => ucfirst($client_info->LastName), "Cnic" => $client_info->Cnic, "MemberID" => "AAA-587", "ProfileImg" => $client_profile_img);
             $companyProfile = array("CompanyName" => "AAA Associates", "WhatsAppNo" => "090078601", "Support" => "+9286989768768", "logo" => $company_profile_img);
 
             //$res = array("client" => $clientProfile, "investments" => $projects, "CompanyProfile" => $companyProfile);
-            $res = array("client" => array($clientProfile),"CompanyProfile" => array($companyProfile),"investments" => $projects);
+            $res = array("client" => array($clientProfile),"CompanyProfile" => array($companyProfile),"investments" => $projects,"CompanyProjects"=>$company_active_projects);
         }else{
             $res = array("exception" => "Invelid user or password");
         }
